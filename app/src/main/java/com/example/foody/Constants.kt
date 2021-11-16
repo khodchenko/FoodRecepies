af4bcab4440e8682d0fa7aa165bc8b13a55ed767
khodchenko <khoch3nk0@gmail.com>
2021-11-16 13:35:42 +0100

    added

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "4e3bfd624d1b459681fa7b0910ca2a73"
+    }
+}
