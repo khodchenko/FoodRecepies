32b1e54d500209e2d8d6fbd68322e7baf7f5e4ce
khodchenko <khoch3nk0@gmail.com>
2021-11-20 14:53:37 +0100

    +RecipesAdapter.kt +RecipesDiffUtil.kt implement binding implement DiffUtil

---
+++ app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.foody.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foody.databinding.RecipesRowLayoutBinding
+import com.example.foody.models.FoodRecipe
+import com.example.foody.models.Result
+import com.example.foody.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(result: Result){
+                binding.result = result
+                binding.executePendingBindings()
+            }
+            companion object{
+                fun from(parent: ViewGroup) : MyViewHolder {
+                    val layoutInflater = LayoutInflater.from(parent.context)
+                    val binding = RecipesRowLayoutBinding.inflate(layoutInflater,parent, false)
+                    return  MyViewHolder(binding)
+                }
+            }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+
+    fun setData(newData:FoodRecipe){
+        val recipesDiffUtil = RecipesDiffUtil(recipe,newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipe = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+
+    }
+
+}
